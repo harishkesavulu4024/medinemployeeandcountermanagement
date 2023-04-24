dcc43ad46cd25e6a142298cccc7415f879b19b8d
hkesa00 <Blackhawk@123456>
2023-04-24 18:52:39 +0530

    Initial commit

--- src/main/webapp/app/entities/entity-routing.module.ts
+++ src/main/webapp/app/entities/entity-routing.module.ts
@@ -4,6 +4,16 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'branch',
+        data: { pageTitle: 'Branches' },
+        loadChildren: () => import('./branch/branch.module').then(m => m.BranchModule),
+      },
+      {
+        path: 'address',
+        data: { pageTitle: 'Addresses' },
+        loadChildren: () => import('./address/address.module').then(m => m.AddressModule),
+      },
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ]),
   ],
